6a196a33aa8ae482ecc680eeb00a442dccafee5d
yariplus <tafike@gmail.com>
2015-01-05 11:44:32 -0500

    refactor

--- library.js
+++ library.js
@@ -16,8 +16,11 @@
         mcquery = require('mcquery'),
         srv = require('dns-srv'),
         rcon = require('simple-rcon'),
-        mcping = require('mc-ping'),
         net = require('net'),
+        dns = require('dns'),
+        varint = require("varint"),
+        bufferpack = require("bufferpack"),
+        encoding = require("encoding"),
 		app;
 
 	var Widget = {
@@ -65,94 +68,121 @@
         var serverport = widget.data.serverport || "25565";
         var queryport = widget.data.queryport || "25565";
         
-        var pingdata;
-        if ( widget.data.uselocalhost ) {
-                pingdata = { port: serverport, host: "0.0.0.0" };
-                console.log("Pinging " + "0.0.0.0" + ":" + serverport);
-            }else{
-                pingdata = { port: serverport, host: serverhost };
-                console.log("Pinging " + serverhost + ":" + serverport);
-        }
-        var socket = net.connect(pingdata, function() {
-                var buf = new Buffer(2);
-                buf[0] = 254;
-                buf[1] = 1;
-                socket.write(buf);
-        });
-            
-        socket.setTimeout(1000, function () {
-            console.log("mc-ping failed when connecting to " + serverhost + ":" + serverport);
-            socket.end();
-            
-            var data = {};
-                
-            data.serveronline = false;
-            data.hostname = widget.data.hostname || "Unknown Server";
-            
-            data.serverhost = serverhost;
-            data.hostip = serverport;
-            data.showip = false;
-            data.showportdomain = true;
-            data.showportip = false;
-            
-            html = templates.parse(html, data);
-            callback(null, html);
-        });
+        pingServer();
         
-        socket.on("data", function(data) {
-            var newdata = [];
-            if (data[0] == 255) { // Server's magic response
-                var iszero = false,
-                    y = 0;
-                for (var i =  1; i < data.length; i++) {
-                    if (data[i] === 0 && iszero) { // Separator
-                        if (newdata[y].length > 0)
-                            y++;
-                        newdata[y] = "";
-                        continue;
-                    }
-                    if (newdata[y] === undefined) {
-                        newdata[y] = "";
-                    }
-                    if (data[i] !== 0) {
-                        iszero = false;
-                        var newchar = String.fromCharCode(data[i]);
-                        if (newchar !== undefined) {
-                            newdata[y] = newdata[y] + newchar;
+        function resolveHost() {
+            switch ( serverhost.substring(0,1) ) {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    break;
+                    // Using an IP, do ping.
+                    pingServer();
+                default:
+                    // Using a domain, check for SRV record.
+                    dns.resolve( "_minecraft._tcp." + serverhost, 'SRV', function (err, addresses) {
+                        if ( err ) {
+                            console.info("SRV lookup failed for " + serverhost);
+                        }else{
+                            console.info("SRV lookup for " + serverhost);
+                            console.info(addresses);
                         }
-                    } else {
-                        iszero = true;
-                    }
-                }
-                if (callback !== undefined) {
-                    data = {};
-                    data.protocol_version = newdata[1];
-                    data.minecraft_version = newdata[2];
-                    data.server_name = newdata[3];
-                    data.num_players = newdata[4];
-                    data.max_players = newdata[5];
-                    //callback(null, data);
-                    console.log("GOT DATA");
-                    console.log(data);
-                    queryServer();
-                }
-            } else {
-                console.log("Unexpected data from server");
+                    });
+                    pingServer();
             }
-            socket.end();
-        });
-
-        socket.once('error', function(e) {
-            if (callback !== undefined) {
-                
-            }
-        });
+        }
         
-        //var sock = new net.Stream();
-        //srv.connect(sock, ['_minecraft._tcp'], serverhost, serverport);
+        function pingServer() {
+            var pingdata;
+            if ( widget.data.uselocalhost ) {
+                    pingdata = { port: serverport, host: "0.0.0.0" };
+                    console.log("Pinging " + "0.0.0.0" + ":" + serverport);
+                }else{
+                    pingdata = { port: serverport, host: serverhost };
+                    console.log("Pinging " + serverhost + ":" + serverport);
+            }
+            var socket = net.connect(pingdata, function() {
+                    var buf = new Buffer(2);
+                    buf[0] = 254;
+                    buf[1] = 1;
+                    socket.write(buf);
+            });
+                
+            socket.setTimeout(1000, function () {
+                console.log("mc-ping failed when connecting to " + serverhost + ":" + serverport);
+                socket.end();
+                
+                var data = {};
+                    
+                data.serveronline = false;
+                data.hostname = widget.data.hostname || "Unknown Server";
+                
+                data.serverhost = serverhost;
+                data.hostip = serverport;
+                data.showip = false;
+                data.showportdomain = true;
+                data.showportip = false;
+                
+                html = templates.parse(html, data);
+                callback(null, html);
+            });
+            
+            socket.on("data", function(data) {
+                var newdata = [];
+                if (data[0] == 255) { // Server's magic response
+                    var iszero = false,
+                        y = 0;
+                    for (var i =  1; i < data.length; i++) {
+                        if (data[i] === 0 && iszero) { // Separator
+                            if (newdata[y].length > 0)
+                                y++;
+                            newdata[y] = "";
+                            continue;
+                        }
+                        if (newdata[y] === undefined) {
+                            newdata[y] = "";
+                        }
+                        if (data[i] !== 0) {
+                            iszero = false;
+                            var newchar = String.fromCharCode(data[i]);
+                            if (newchar !== undefined) {
+                                newdata[y] = newdata[y] + newchar;
+                            }
+                        } else {
+                            iszero = true;
+                        }
+                    }
+                    if (callback !== undefined) {
+                        data = {};
+                        data.protocol_version = newdata[1];
+                        data.minecraft_version = newdata[2];
+                        data.server_name = newdata[3];
+                        data.num_players = newdata[4];
+                        data.max_players = newdata[5];
+                        //callback(null, data);
+                        console.log("GOT DATA");
+                        console.log(data);
+                        queryServer();
+                    }
+                } else {
+                    console.log("Unexpected data from server");
+                }
+                socket.end();
+            });
 
-        //sock.on('error', function() { console.log('SRV lookup error.'); })
-        //    .on('connect', function() { console.log("SRV " + port); });
+            socket.once('error', function(e) {
+                if (callback !== undefined) {
+                    // ???
+                }
+            });
+        }
         
         function queryServer() {
             var query;
@@ -256,22 +286,6 @@
                     if ( !data.showportdomain && data.showportip ) {
                         data.showportdomain = true;
                     }
-                }
-                
-                switch ( data.serverhost.substring(0,1) ) {
-                    case "0":
-                    case "1":
-                    case "2":
-                    case "3":
-                    case "4":
-                    case "5":
-                    case "6":
-                    case "7":
-                    case "8":
-                    case "9":
-                        break;
-                    default:
-                        // Using a domain
                 }
                 
                 data.queryonline = true;
