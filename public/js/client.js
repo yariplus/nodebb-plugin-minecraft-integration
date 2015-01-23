2853474382976fcf9a0cb92ad10958c7f0e19c37
yariplus <tafike@gmail.com>
2015-01-23 04:09:16 -0500

    Fix derp

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,11 @@
-"use strict";
 /*global ajaxify*/
 
 // I've seen the other side of rainbow
 // And it was black and white
 
-$.getScript("/vendor/chart.js/chart.min.js?v=v0.6.0");
+if (!Chart) {
+	$.getScript("/vendor/chart.js/chart.min.js?v=v0.6.0");
+}
 
 $(function () {
     $('body').tooltip({
@@ -33,7 +34,7 @@ function resizeend() {
         timeout = false;
 		resizeCanvases();
     }               
-}
+};
 
 function resizeCanvases() {
 	$('.canvasResizable').each(function(index){
@@ -43,13 +44,15 @@ function resizeCanvases() {
 		var options = window[$(this).attr('id') + 'Options'];
 		var chart = new Chart($(this)[index].getContext('2d')).Line(data, options);
 	});
-}
+};
 
 $(document).ajaxComplete(function(event, response, settings) {
 	if (!!~settings.url.indexOf("/api/widgets/render")) {
 		$('.widgetFillContainer').each(function(index){
 			var parent = $(this).parent();
-			if (!$(parent).prop('widget-area')) $(parent).css('padding-top', '0').css('padding-left', '0').css('padding-right', '0').css('padding-bottom', '0');
+			if (!$(parent).prop('widget-area')) {
+				$(parent).css('padding-top', '0').css('padding-left', '0').css('padding-right', '0').css('padding-bottom', '0');
+			}
 		});
 		resizeCanvases();
 	}
