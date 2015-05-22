35bbb998dc450b40b52542eed29a95b137ca8afd
yariplus <tafike@gmail.com>
2015-05-22 01:19:43 -0400

    Add more hackishness.
    Fancy title formating, fix new widgets not ajaxing properly when added.

--- public/js/client.js
+++ public/js/client.js
@@ -146,11 +146,29 @@ $(window).on('action:widgets.loaded', function (event, data) {
 	});
 });
 
+var miIDcounter = 1;
+
 $(window).on('action:ajaxify.end', function (event, url) {
 	url = url.url.split('?')[0].split('#')[0];
 
 	console.log('ajaxify.end: ' + url);
 
+	switch (url) {
+		case 'admin/extend/widgets':
+			setTimeout(function(){ $(window).trigger('action:widgets.adminDataLoaded'); }, 5000);
+			break;
+	}
+});
+
+$(window).on('action:ajaxify.contentLoaded', function (event, data) {
+	console.log("contentLoaded: " + JSON.stringify(data));
+});
+
+$(window).on('action:ajaxify.dataLoaded', function (event, data) {
+	console.log("dataLoaded: " + JSON.stringify(data));
+});
+
+$(window).on('action:widgets.adminDataLoaded', function (event, data) {
 	function formatTitle($panel) {
 		var $title = $panel.find('>.panel-heading strong'),
 			title = $panel.find('>.panel-body [name="title"]').val();
@@ -162,98 +180,119 @@ $(window).on('action:ajaxify.end', function (event, url) {
 
 		title = title.replace(/\{\{motd\}\}/g, $panel.data('motd'));
 		title = title.replace(/\{\{name\}\}/g, $panel.data('name'));
-		title = title.replace(/[§&][0123456789abcdefklmnor]/g, '');
+		title = title.replace(/[\u0247&][0123456789abcdefklmnor]/g, '');
 
 		$title.html($title.text().split(' - ')[0] + ' - ' + title);
 	}
 
-	function initACPWidgets() {
-		var IDcounter = 1;
+	function initPanel($panel) {
+		var $heading = $panel.find('>.panel-heading'),
+			$body = $heading.next();
 
-		$('.widget-area').on('mouseup', '> .panel > .panel-heading', function (e) {
-			var $heading = $(this),
-				$panel = $heading.parent(),
-				$body = $heading.next(),
-				widget = $panel.data('widget');
+		if ($body.hasClass('mcwe-ajaxed')) {
+			return;
+		}else{
+			$body.addClass('mcwe-ajaxed');
+		}
 
-			if ($(this).parent().is('.ui-sortable-helper') || $(e.target).closest('.delete-widget').length) return;
+		if ($body.find('[name="sid"]').val()) {
+			$.get('/api/minecraft-integration/server/' + $body.find('[name="sid"]').val() + "?v=" + config['cache-buster'], function (server) {
+				if (server && server.motd && server.name) {
+					$panel.data('motd', server.motd);
+					$panel.data('name', server.name);
+				}else{
+					$panel.data('motd', '');
+					$panel.data('name', '');
+				}
+				formatTitle($panel);
+			});
+		}
 
-			switch (widget) {
-				case 'widgetMCServerStatus':
-					return (function () {
-						if ( !$body.hasClass('mcwe-ajaxed') ) {
-							$body.addClass('mcwe-ajaxed');
-
-							$.get('/api/minecraft-integration/server/' + $body.find('[name="sid"]').val() + "?v=" + config['cache-buster'], function (server) {
-								if (server && server.motd && server.name) {
-									$panel.data('motd', server.motd);
-									$panel.data('name', server.name);
-									formatTitle($panel);
-								}else{
-									$panel.data('motd', '');
-									$panel.data('name', '');
-								}
-
-								$body.find('[name="title"]').on('input', function (e) {
-									formatTitle($panel);
-								});
-
-								$body.find('input.ajaxSelectSibling').each(function(index){
-									var MCWESN = $(this);
-									if (MCWESN.val()) {
-										MCWESN.prev().val($(this).val());
-									}else{
-										var first = MCWESN.prev().find('option:first'),
-											selected = MCWESN.prev().find('option:selected');
-										if (selected) {
-											MCWESN.val( selected.val() );
-										}else{
-											if (first) {
-												MCWESN.val( first.val() );
-												MCWESN.prev().val( first.val() );
-											}
-										}
-									}
-									MCWESN.prev().on('change', function(){
-										MCWESN.val($(this).val());
-									});
-								});
-								$body.find('input.ajaxInputColorPicker').each(function(index){
-									if ($(this).val() === '') $(this).val('000000');
-									var MCWECP = $(this);
-									var id = 'ajaxInputColorPicker' + IDcounter;
-									MCWECP.attr('id',id);
-									$('#'+id).ColorPicker({
-										color: MCWECP.val() || '#000000',
-										onChange: function(hsb, hex) {
-											MCWECP.val(hex);
-											MCWECP.css('color', '#' + hex);
-
-											if(MCWECP.is('[preview]')) {
-												MCWECP.parents('.panel-body').find('.mcWidgetPreview').find(MCWECP.attr('preview')).each(function(){$(this).css('color', '#' + MCWECP.val())});
-											}
-										},
-										onShow: function(colpkr) {
-											$(colpkr).css('z-index', 1051);
-										}
-									}).css('color', '#' + $(this).val()).bind('keyup', function(){
-										$(this).ColorPickerSetColor($(this).val());
-										$(this).css('color', '#' + $(this).val());
-									});
-									if(MCWECP.is('[preview]')) {
-										MCWECP.parents('.panel-body').find('.mcWidgetPreview').find(MCWECP.attr('preview')).each(function(){$(this).css('color', '#' + MCWECP.val())});
-									}
-									IDcounter++;
-								});
-							});
-						}
-					}());
-			}
+		$body.find('[name="title"]').on('input', function (e) {
+			formatTitle($panel);
 		});
-	};
 
-	switch (url) {
-		case 'admin/extend/widgets':
-			return initACPWidgets();
+		$body.find('input.ajaxSelectSibling').each(function(index){
+			var MCWESN = $(this);
+			if (MCWESN.val()) {
+				MCWESN.prev().val($(this).val());
+			}else{
+				var first = MCWESN.prev().find('option:first'),
+					selected = MCWESN.prev().find('option:selected');
+				if (selected) {
+					MCWESN.val( selected.val() );
+				}else{
+					if (first) {
+						MCWESN.val( first.val() );
+						MCWESN.prev().val( first.val() );
+					}
+				}
+			}
+			MCWESN.prev().on('change', function(){
+				MCWESN.val($(this).val());
+				$.get('/api/minecraft-integration/server/' + $body.find('[name="sid"]').val() + "?v=" + config['cache-buster'], function (server) {
+					if (server && server.motd && server.name) {
+						$panel.data('motd', server.motd);
+						$panel.data('name', server.name);
+					}else{
+						$panel.data('motd', '');
+						$panel.data('name', '');
+					}
+					formatTitle($panel);
+				});
+			});
+		});
+
+		$body.find('input.ajaxInputColorPicker').each(function(index){
+			if ($(this).val() === '') $(this).val('000000');
+			var MCWECP = $(this);
+			var id = 'ajaxInputColorPicker' + miIDcounter;
+			MCWECP.attr('id',id);
+			$('#'+id).ColorPicker({
+				color: MCWECP.val() || '#000000',
+				onChange: function(hsb, hex) {
+					MCWECP.val(hex);
+					MCWECP.css('color', '#' + hex);
+
+					if(MCWECP.is('[preview]')) {
+						MCWECP.parents('.panel-body').find('.mcWidgetPreview').find(MCWECP.attr('preview')).each(function(){$(this).css('color', '#' + MCWECP.val())});
+					}
+				},
+				onShow: function(colpkr) {
+					$(colpkr).css('z-index', 1051);
+				}
+			}).css('color', '#' + $(this).val()).bind('keyup', function(){
+				$(this).ColorPickerSetColor($(this).val());
+				$(this).css('color', '#' + $(this).val());
+			});
+			if(MCWECP.is('[preview]')) {
+				MCWECP.parents('.panel-body').find('.mcWidgetPreview').find(MCWECP.attr('preview')).each(function(){$(this).css('color', '#' + MCWECP.val())});
+			}
+			miIDcounter++;
+		});
 	}
+
+	$('.widget-area').on('mouseup', '> .panel > .panel-heading', function (e) {
+		var $heading = $(this),
+			$panel = $heading.parent(),
+			$body = $heading.next(),
+			widget = $panel.data('widget');
+
+		if ($heading.parent().is('.ui-sortable-helper') || $(e.target).closest('.delete-widget').length) return;
+
+		console.log("CLICK on " + widget);
+		switch (widget) {
+			case 'widgetMCServerStatus':
+				initPanel($panel);
+				break;
+		}
+	});
+
+	$('.widget-area >[data-widget]').each(function (i, el) {
+		switch ($(el).data('widget')) {
+			case 'widgetMCServerStatus':
+				initPanel($(el));
+				break;
+		}
+	});
 });
