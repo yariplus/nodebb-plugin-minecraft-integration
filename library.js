c74dd017919a0d2fa08ac904695349443d80ff30
yariplus <tafike@gmail.com>
2016-07-12 19:51:38 -0400

    Reducing compatibility to node 4

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 
 var analytics = require('./lib/analytics');
 
-var	Admin   = require('./lib/admin');
+var Admin   = require('./lib/admin');
 var API     = require('./lib/api');
 var Backend = require('./lib/backend');
 var Config  = require('./lib/config');
@@ -14,46 +14,46 @@ var Views   = require('./lib/views');
 var routes = require('./lib/routes');
 
 var MinecraftIntegration = module.exports = {
-	Widgets: require('./lib/widgets'),
-	Hooks: require('./lib/hooks')
+  Widgets: require('./lib/widgets'),
+  Hooks: require('./lib/hooks')
 };
 
 NodeBB.emitter.once('nodebb:ready', Views.modifyTemplates);
 
 MinecraftIntegration.load = function (params, next) {
 
-	NodeBB.app        = params.app;
-	NodeBB.router     = params.router;
-	NodeBB.middleware = params.middleware;
+  NodeBB.app        = params.app;
+  NodeBB.router     = params.router;
+  NodeBB.middleware = params.middleware;
 
-	NodeBB.app.set('json spaces', 4);
+  NodeBB.app.set('json spaces', 4);
 
-	NodeBB.init();
-	API.init();
-	Views.init();
-	Admin.init();
+  NodeBB.init();
+  API.init();
+  Views.init();
+  Admin.init();
 
-	routes();
+  routes();
 
-	// Add a default server.
-	NodeBB.db.getObject('mi:server:0:config', function (err, config) {
+  // Add a default server.
+  NodeBB.db.getObject('mi:server:0:config', function (err, config) {
 
-		if (err) return next(new Error(err));
+    if (err) return next(new Error(err));
 
-		config = config || {};
-		config.name        = config.name        || "A Minecraft Server";
-		config.address     = config.address     || (require('nconf').get('url') + ":25565");
-		config.APIKey      = config.APIKey      || Utils.getKey(),
-		config.hidePlugins = config.hidePlugins || "0";
+    config = config || {};
+    config.name        = config.name        || "A Minecraft Server";
+    config.address     = config.address     || (require('nconf').get('url') + ":25565");
+    config.APIKey      = config.APIKey      || Utils.getKey(),
+    config.hidePlugins = config.hidePlugins || "0";
 
-		NodeBB.db.setObject('mi:server:0:config', config);
-		NodeBB.db.sortedSetAdd('mi:servers', Date.now(), '0');
+    NodeBB.db.setObject('mi:server:0:config', config);
+    NodeBB.db.sortedSetAdd('mi:servers', Date.now(), '0');
 
-		setTimeout(Config.logSettings, 5000);
-		setTimeout(Updater.updateServers, 10000);
+    setTimeout(Config.logSettings, 5000);
+    setTimeout(Updater.updateServers, 10000);
 
-		next();
+    next();
 
-	});
+  });
 
 };
